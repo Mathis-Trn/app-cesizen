6af48cd864670ada56baecae3ad6d21df6620907
Mathis Ternon <mathis.ternon@devinfluence.fr>
2025-05-02 22:53:21 +0200

    Feat: Add Logo

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -6,6 +6,7 @@ import { useFonts } from "expo-font";
 import CustomDrawerMenu from "../components/CustomDrawerMenu";
 import { useEffect } from "react";
 import * as SplashScreen from 'expo-splash-screen';
+import { Image } from "react-native";
 
 const DrawerLayout = () => {
   const [fontsLoaded] = useFonts({
@@ -27,7 +28,6 @@ const DrawerLayout = () => {
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
       <Drawer drawerContent={CustomDrawerMenu} screenOptions={{
-        drawerHideStatusBarOnOpen: false,
         drawerActiveBackgroundColor: "#28BF37",
         drawerActiveTintColor: "#fff",
         drawerLabelStyle: {
@@ -35,38 +35,44 @@ const DrawerLayout = () => {
           fontSize: 15,
           fontFamily: "Inter"
         },
+        drawerType: "slide",
+        drawerHideStatusBarOnOpen: false,
+        headerTitle: () => (
+          <Image
+            source={require('../assets/images/logo-cesizen.png')}
+            style={{ width: 120, height: 40, resizeMode: 'contain' }}
+          />
+        ),
+        headerTitleAlign: 'center',
+        swipeEdgeWidth: 70,
+        headerTintColor: "#28BF37",
       }}>
         <Drawer.Screen name="index" options={{ 
           drawerLabel: "Accueil",
-          headerTitle: "Accueil",
           drawerIcon: ({ size, color}) => (
             <Ionicons name="home-outline" size={size} color={color} />
           )
         }} />
         <Drawer.Screen name="informations" options={{ 
           drawerLabel: "Articles d'information",
-          headerTitle: "Articles d'information",
           drawerIcon: ({ size, color}) => (
             <Ionicons name="newspaper-outline" size={size} color={color} />
           )
         }} />
         <Drawer.Screen name="activities" options={{ 
           drawerLabel: "Activités",
-          headerTitle: "Activités",
           drawerIcon: ({ size, color}) => (
             <Ionicons name="barbell-outline" size={size} color={color} />
           )
         }} />
         <Drawer.Screen name="profile" options={{ 
           drawerLabel: "Profil",
-          headerTitle: "Profil",
           drawerIcon: ({ size, color}) => (
             <Ionicons name="person-outline" size={size} color={color} />
           )
         }} />
         <Drawer.Screen name="settings" options={{ 
           drawerLabel: "Paramètres",
-          headerTitle: "Paramètres",
           drawerIcon: ({ size, color}) => (
             <Ionicons name="settings-outline" size={size} color={color} />
           )
